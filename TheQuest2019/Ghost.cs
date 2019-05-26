6ab8f926f3bc487b34b0df64fb32faff38b8f73b
Mark <Mark@DESKTOP-16DN5B4>
2019-05-26 16:16:14 +0200

    add implementation to ghost and ghoul

--- TheQuest2019/Ghost.cs
+++ TheQuest2019/Ghost.cs
@@ -1,13 +1,34 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace TheQuest2019
 {
     internal class Ghost : Enemy
     {
         public Ghost(Game game, Point location)
-            :base(game, location)
+            :base(game, location, hitpoints: 8) // ghost has 8 hitpoints
         {
             //
+        }
+
+        public override void Move(Random random)
+        {
+            if (Hitpoints > 0)
+            {
+                // 1 in 3 chance move towards player
+                if (random.Next(3) == 0) // move towards player
+                {
+                    base.Move(FindPlayerDirection(game.PlayerLocation), game.Boundaries);
+                }
+                else
+                {
+                    // stand still
+                }
+                if (NearPlayer())
+                {
+                    game.HitPlayer(3, random); // ghost attacks with 2
+                }
+            }
         }
     }
 }
--- TheQuest2019/Ghoul.cs
+++ TheQuest2019/Ghoul.cs
@@ -1,13 +1,34 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace TheQuest2019
 {
     internal class Ghoul : Enemy
     {
         public Ghoul(Game game, Point location)
-            :base(game, location)
+            :base(game, location, hitpoints: 10) // ghoul has 10 hitpoints
         {
             //
+        }
+
+        public override void Move(Random random)
+        {
+            if (Hitpoints > 0)
+            {
+                // 2 in 3 chance move towards player
+                if (random.Next(3) == 0)
+                {
+                    // stand still
+                }
+                else // move towards player
+                {
+                    base.Move(FindPlayerDirection(game.PlayerLocation), game.Boundaries);
+                }
+                if (NearPlayer())
+                {
+                    game.HitPlayer(4, random); // ghost attacks with 2
+                }
+            }
         }
     }
 }
